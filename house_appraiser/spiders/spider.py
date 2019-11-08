779cf11c2172550495ce3ea73a169ed8755091fa
Brian Froschauer <brian.froschauer@ing.austral.edu.ar>
2019-11-08 13:45:09 -0300

    improvement(spider): improve parse item method

--- house_appraiser/spiders/spider.py
+++ house_appraiser/spiders/spider.py
@@ -9,7 +9,7 @@ class HouseAppraiserSpider(CrawlSpider):
     name = 'house_appraiser'
     item_count = 0
     allowed_domain = ['https://www.zonaprop.com.ar']
-    start_urls = ['https://www.zonaprop.com.ar/departamentos-venta-pagina-1.html']
+    start_urls = ['https://www.zonaprop.com.ar/casas-venta.html']
     rules = {
         Rule(LinkExtractor(allow=(), restrict_xpaths=('//li[@class="pag-go-next"]/a'))),
         Rule(LinkExtractor(allow=(), restrict_xpaths=('//h3[@class="posting-title"]')),
@@ -17,24 +17,53 @@ class HouseAppraiserSpider(CrawlSpider):
     }
 
     def parse_item(self, response):
-        item = HouseAppraiserItem()
+        house = HouseAppraiserItem()
 
-        item['precio'] = response.xpath('normalize-space(//*[@id="sidebar-price-container"]/div/div[1]/div[2])').extract()
-        item['superficie_total'] = response.xpath('normalize-space(//*[@id="article-container"]/section[1]/ul/li[1]/b)').extract()
-        item['superficie_cubierta'] = response.xpath('normalize-space(//*[@id="article-container"]/section[1]/ul/li[2]/b)').extract()
-        item['ambientes'] = response.xpath('normalize-space(//*[@id="article-container"]/section[1]/ul/li[3]/b)').extract()
-        item['baños'] = response.xpath('normalize-space(//*[@id="article-container"]/section[1]/ul/li[4]/b)').extract()
-        item['cocheras'] = response.xpath('normalize-space(//*[@id="article-container"]/section[1]/ul/li[5]/b)').extract()
-        item['dormitorios'] = response.xpath('normalize-space(//*[@id="article-container"]/section[1]/ul/li[6]/b)').extract()
-        item['toilettes'] = response.xpath('normalize-space(//*[@id="article-container"]/section[1]/ul/li[7]/b)').extract()
-        item['antiguedad'] = response.xpath('normalize-space(//*[@id="article-container"]/section[1]/ul/li[8]/b)').extract()
-        item['disposicion'] = response.xpath('normalize-space(//*[@id="article-container"]/section[1]/ul/li[9]/b)').extract()
-        item['orientacion'] = response.xpath('normalize-space(//*[@id="article-container"]/section[1]/ul/li[10]/b)').extract()
-        item['estado'] = response.xpath('normalize-space(//*[@id="article-container"]/section[1]/ul/li[11]/b)').extract()
-        item['luminosidad'] = response.xpath('normalize-space(//*[@id="article-container"]/section[1]/ul/li[12]/b)').extract()
+        items = response.xpath('/html/body/div[1]/main/div/div/article/div/section[1]/ul')
+
+        for item in items:
+
+            house['precio'] = item.xpath('/html/body/div[1]/main/div/div/aside/div/div[1]/div/div[1]/div[2]/span/text()').extract()
+
+            for i in range(1, 12):
+
+                label = item.xpath('li['+str(i)+']/span/text()').extract_first()
+
+                if label == 'Superficie total':
+                    house['superficie_total'] = item.xpath('li['+str(i)+']/b/text()').extract_first()
+
+                elif label == 'Superficie cubierta':
+                    house['superficie_cubierta'] = item.xpath('li['+str(i)+']/b/text()').extract_first()
+
+                elif label == 'Ambientes':
+                    house['ambientes'] = item.xpath('li['+str(i)+']/b/text()').extract_first()
+
+                elif label == 'Baños':
+                    house['baños'] = item.xpath('li['+str(i)+']/b/text()').extract_first()
+
+                elif label == 'Cochera':
+                    house['cocheras'] = item.xpath('li['+str(i)+']/b/text()').extract_first()
+
+                elif label == 'Dormitorios':
+                    house['dormitorios'] = item.xpath('li['+str(i)+']/b/text()').extract_first()
+
+                elif label == 'Toilette':
+                    house['toilettes'] = item.xpath('li['+str(i)+']/b/text()').extract_first()
+
+                elif label == 'Antigüedad':
+                    house['antiguedad'] = item.xpath('li['+str(i)+']/b/text()').extract_first()
+
+                elif label == 'Orientación':
+                    house['orientacion'] = item.xpath('li['+str(i)+']/b/text()').extract_first()
+
+                elif label == 'Estado del inmueble':
+                    house['estado'] = item.xpath('li['+str(i)+']/b/text()').extract_first()
+
+                elif label == 'Luminosidad':
+                    house['luminosidad'] = item.xpath('li['+str(i)+']/b/text()').extract_first()
 
         self.item_count += 1
-        if self.item_count > 1000:
+        if self.item_count > 5000:
             raise CloseSpider('item_exceeded')
-        yield item
+        yield house
 
